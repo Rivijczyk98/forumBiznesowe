e0208242e71538cf923e89bf70617777b4ee0173
Daniel Demczuk <daniel.demczuk.28@gmail.com>
2020-06-10 12:46:35 +0200

    initial commit

---
+++ forum-biznesowe/src/main/java/pl/edu/pb/wi/forumbiznesowe/ForumBiznesoweApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.wi.forumbiznesowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumBiznesoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumBiznesoweApplication.class, args);
+	}
+
+}
