5c13eab52ca5f65da0f5cb2c020d446a9734fc7e
Pawel <mrbob724@gmail.com>
2020-06-19 01:11:48 +0200

    WIP

---
+++ forumAngularclient/src/app/_services/report.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Report} from '../_model/report';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportService {
+
+  constructor(http: HttpClient) {
+  }
+
+  addReport(report: Report) {
+
+  }
+}
